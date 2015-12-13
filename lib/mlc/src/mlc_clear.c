03f2a9995424347bb1eb5d0504389207d13d1a66
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2015-12-13 11:03:49 +0100

    better clear function

--- lib/mlc/src/mlc_clear.c
+++ lib/mlc/src/mlc_clear.c
@@ -7,7 +7,21 @@
 
 int	mlc_clear()
 {
+  int   x, y;
+  t_mlc *mlc;
+
+  mlc = mlc_link(0);
   if (write(2, TRM_CL, sizeof(TRM_CL) - 1) < 0)
     return (-1);
+  y = -1;
+  while (++y < mlc->h)
+  {
+      x = -1;
+      while(++x < mlc->w)
+      {
+          mlc->color[y][x] = 0;
+          mlc->buff[y][x] = 0;
+      }
+  }
   return (0);
 }
